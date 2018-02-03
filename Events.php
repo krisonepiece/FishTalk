1dfdc346c9bb46e865d780c6eaf2e1d81d1a9717
tom830001 <36037826+tom830001@users.noreply.github.com>
2018-02-03 22:49:58 +0800

    get introduction

--- Events.php
+++ Events.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * This file is part of workerman.
  *
@@ -11,20 +11,17 @@
  * @link http://www.workerman.net/
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 /**
  * 用于检测业务代码死循环或者长时间阻塞等问题
  * 如果发现业务卡死，可以将下面declare打开（去掉//注释），并执行php start.php reload
  * 然后观察一段时间workerman.log看是否有process_timeout异常
  */
 //declare(ticks=1);
-
 /**
  * 聊天主逻辑
  * 主要是处理 onMessage onClose 
  */
 use \GatewayWorker\Lib\Gateway;
-
 class Events
 {
    /**
@@ -53,33 +50,20 @@ class Events
             // 客户端登录 message格式: {type:login, name:xx, room_id:1} ，添加到客户端，广播给所有客户端xx进入聊天室
             case 'login':
                 // 判断是否有房间号
-                /*
                 if(!isset($message_data['room_id']))
                 {
                     throw new \Exception("\$message_data['room_id'] not set. client_ip:{$_SERVER['REMOTE_ADDR']} \$message:$message");
-                }*/
+                }
                 
                 // 把房间号昵称放到session中
-                //$room_id = $message_data['room_id'];
+				$a = array();
+                $room_id = $message_data['room_id'];
                 $client_name = htmlspecialchars($message_data['client_name']);
-                //$_SESSION['room_id'] = $room_id;
+                $_SESSION['room_id'] = $room_id;
                 $_SESSION['client_name'] = $client_name;
-                
-                $a = array();
-                //$room_id = $message_data['room_id'];
-                $client_name = htmlspecialchars($message_data['client_name']);
-                //$_SESSION['room_id'] = $room_id;
-                $_SESSION['client_name'] = $client_name;
-                
-                //取得所有目前有上限的client id, name
-                $clients_list = Gateway::getAllClientSessions();
-                foreach($clients_list as $tmp_client_id=>$item)
-                {
-                    $global_list[$item['client_name']] = $tmp_client_id;
-                }
-                $global_list[$client_name] = $client_id;
 				
-				$servername = "140.117.169.140";
+				
+				$servername = "localhost";
 				$username = "fishtalk";
 				$password = "fish2018";
 				$dbname = "fishtalk";
@@ -100,101 +84,80 @@ class Events
 				} else {
 					echo "0 results";
 				}
-                $conn->close();
-                
-                
+				$conn->close();
+				////獲得INTRODUCTION
+				$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die('Error with MySQL connection');
+				$sql = "SELECT * FROM `member` WHERE account='".$_SESSION['client_name']."' ";//查詢整個表單
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+				// output data of each row
+				while($row = $result->fetch_assoc()) {
+					echo $row["introduction"];
+					$introduction=array('type'=>'get_introduction', 'name'=>$_SESSION['client_name'], 'introduction'=>$row["introduction"]);
+				}
+				} else {
+					echo "0 results";
+				}
+					//echo $introduction["introduction"];
+				$conn->close();
+				
+				////
+				//$array[] = $var;
                 // 获取房间内所有用户列表 
-                /*
                 $clients_list = Gateway::getClientSessionsByGroup($room_id);
                 foreach($clients_list as $tmp_client_id=>$item)
                 {
                     $clients_list[$tmp_client_id] = $item['client_name'];
                 }
-                $clients_list[$client_id] = $client_name;*/
+                $clients_list[$client_id] = $client_name;
                 
                 // 转播给当前房间的所有客户端，xx进入聊天室 message {type:login, client_id:xx, name:xx} 
                 $new_message = array('type'=>$message_data['type'], 'client_id'=>$client_id, 'client_name'=>htmlspecialchars($client_name), 'time'=>date('Y-m-d H:i:s'));
-                //Gateway::sendToGroup($room_id, json_encode($new_message));
-                //Gateway::joinGroup($client_id, $room_id);
-                $list_message = array('type'=>'global_list', 'global_client_list'=>$global_list);
+                Gateway::sendToGroup($room_id, json_encode($new_message));
+				Gateway::sendToGroup($room_id, json_encode($introduction));
+                Gateway::joinGroup($client_id, $room_id);
+               
                 // 给当前用户发送用户列表 
-                $new_message['client_list'] = $a;
+                $new_message['client_list'] = $clients_list;
+				$new_message['friend_list'] = $a;
                 Gateway::sendToCurrentClient(json_encode($new_message));
-                Gateway::sendToAll(json_encode($list_message));
                 return;
                 
             // 客户端发言 message: {type:say, to_client_id:xx, content:xx}
-            case 'online_message':
-                echo "id: ".message_data["to_client_id"]. " msg:". message_data["content"];
-                $client_name = $_SESSION['client_name'];
-                $to_client_id = $message_data['to_client_id'];
-                $talk_name = $message_data['to_client'];
-                $content = $message_data['content'];
-
-                $online_message = array('type'=>'online_message', 'from_client_id'=>$client_id, 'from_client_name'=>$client_name,'content'=>$content);
-                Gateway::sendToClient($to_client_id, json_encode($online_message));
-
-                $servername = "140.117.169.140";
-				$username = "fishtalk";
-				$password = "fish2018";
-				$dbname = "fishtalk";
-				$conn = new mysqli($servername, $username, $password, $dbname);
-				if ($conn->connect_error) {
-					die("Connection failed: " . $conn->connect_error);
-				} 
-				$sql = "INSERT INTO `content` (`account_A`, `account_B`, `message`) VALUES ('$client_name', '$talk_name', '$content')" ;
-				if ($conn->query($sql) === TRUE) {
-					echo "New record created successfully";
-				} else {
-					echo "Error: " . $sql . "<br>" . $conn->error;
-				}
-                $conn->close();
-                return;
-
-            case 'offline_message':            
-                $client_name = $_SESSION['client_name'];
-                $talk_name = $message_data['to_client'];
-                $content = $message_data['content'];
-
-                $servername = "140.117.169.140";
-				$username = "fishtalk";
-				$password = "fish2018";
-				$dbname = "fishtalk";
-				$conn = new mysqli($servername, $username, $password, $dbname);
-				if ($conn->connect_error) {
-					die("Connection failed: " . $conn->connect_error);
-				} 
-				$sql = "INSERT INTO `content` (`account_A`, `account_B`, `message`) VALUES ('$client_name', '$talk_name', '$content')" ;
-				if ($conn->query($sql) === TRUE) {
-					echo "New record created successfully";
-				} else {
-					echo "Error: " . $sql . "<br>" . $conn->error;
-				}
-                $conn->close();
-                return;
-
-            case 'require_content':
-                $talk_name = $message_data['to_client']; //account_B
-                $a = array();
-                $_SESSION['talkname_name'] = $talk_name;
-                $servername = "140.117.169.140";
-                $username = "fishtalk";
-                $password = "fish2018";
-                $dbname = "fishtalk";
-                
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                $sql = 'SELECT * FROM content WHERE (account_A = "'.$_SESSION['client_name'].'" AND account_B = "'.$_SESSION['talkname_name'].'") OR (account_B = "'.$_SESSION['client_name'].'" AND account_A = "'.$_SESSION['talkname_name'].'")  ';
-                $result = $conn->query($sql);
-                while($row = $result->fetch_array())
+            case 'say':
+                // 非法请求
+                if(!isset($_SESSION['room_id']))
                 {
-                    $a[] = $row;
+                    throw new \Exception("\$_SESSION['room_id'] not set. client_ip:{$_SERVER['REMOTE_ADDR']}");
                 }
-                $conn->close();
-                $new_message = array('type'=>$message_data['type'], 'client_id'=>$client_id, 'client_name'=>htmlspecialchars($client_name), 'time'=>date('Y-m-d H:i:s'));
-                $new_message['content_entity'] = $a;
-                Gateway::sendToCurrentClient(json_encode($new_message));
-                echo $a[0];
-                return;
+                $room_id = $_SESSION['room_id'];
+                $client_name = $_SESSION['client_name'];
+                
+                // 私聊
+                if($message_data['to_client_id'] != 'all')
+                {
+                    $new_message = array(
+                        'type'=>'say',
+                        'from_client_id'=>$client_id, 
+                        'from_client_name' =>$client_name,
+                        'to_client_id'=>$message_data['to_client_id'],
+                        'content'=>"<b>对你说: </b>".nl2br(htmlspecialchars($message_data['content'])),
+                        'time'=>date('Y-m-d H:i:s'),
+                    );
+                    Gateway::sendToClient($message_data['to_client_id'], json_encode($new_message));
+                    $new_message['content'] = "<b>你对".htmlspecialchars($message_data['to_client_name'])."说: </b>".nl2br(htmlspecialchars($message_data['content']));
+                    return Gateway::sendToCurrentClient(json_encode($new_message));
+                }
+                
+                $new_message = array(
+                    'type'=>'say', 
+                    'from_client_id'=>$client_id,
+                    'from_client_name' =>$client_name,
+                    'to_client_id'=>'all',
+                    'content'=>nl2br(htmlspecialchars($message_data['content'])),
+                    'time'=>date('Y-m-d H:i:s'),
+                );
+                return Gateway::sendToGroup($room_id ,json_encode($new_message));
         }
    }
    
